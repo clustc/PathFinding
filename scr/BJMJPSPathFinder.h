c3427fe6b1dfb150847313342b837e49175d7fe9
clustc <clustcer@qq.com>
2017-01-20 18:33:41 +0800

    Create BJMJPSPathFinder.h

---
+++ scr/BJMJPSPathFinder.h
@@ -0,0 +1,37 @@
+/****************************************************************************
+Author : CaiLi @Bojoy 2017
+FileName: 
+Description: 
+****************************************************************************/
+
+#pragma once
+
+#include "BJMPathFinder.h"
+
+namespace BJMGame
+{
+
+class BJMJPSPathFinder : public BJMPathFinder
+{
+public:
+	BJMJPSPathFinder(void);
+	~BJMJPSPathFinder(void);
+
+	GridNodeVec FindNeighbours(GridNode* node);
+
+	//Searches for a jump point (or a turning point) in a specific direction.
+	GridNode* Jump(GridNode* node, GridNode* parent, GridNode* endNode);
+
+	//Searches for successors of a given node in the direction of each of its neighbours.
+	void IdentifySuccessors(GridNodeVec& openList, GridNode* node, GridNode* endNode);
+
+	bool PathFinding(const cocos2d::Point & from, const cocos2d::Point & to);
+
+	//`Path` filling modifier. Interpolates between non contiguous nodes along a `path`
+	void FillPath(GridNode* endNode);
+
+	static BJMJPSPathFinder* Create(int width, int height);
+
+};
+
+}
