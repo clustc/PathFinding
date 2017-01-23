af1f843e3709745b123f7928962282b5f442ccb4
clustc <clustcer@qq.com>
2017-01-23 19:50:28 +0800

    Update BJMJPSPathFinder.h

--- scr/BJMJPSPathFinder.h
+++ scr/BJMJPSPathFinder.h
@@ -17,7 +17,7 @@ public:
 	BJMJPSPathFinder(void);
 	~BJMJPSPathFinder(void);
 
-	GridNodeVec FindNeighbours(GridNode* node);
+	void FindNeighbours(GridNodeVec& output, GridNode* node);
 
 	//Searches for a jump point (or a turning point) in a specific direction.
 	GridNode* Jump(GridNode* node, GridNode* parent, GridNode* endNode);
@@ -25,7 +25,7 @@ public:
 	//Searches for successors of a given node in the direction of each of its neighbours.
 	void IdentifySuccessors(GridNodeVec& openList, GridNode* node, GridNode* endNode);
 
-	bool PathFinding(const cocos2d::Point & from, const cocos2d::Point & to);
+	BJMPath* PathFinding(const cocos2d::Point & from, const cocos2d::Point & to);
 
 	//`Path` filling modifier. Interpolates between non contiguous nodes along a `path`
 	void FillPath(GridNode* endNode);
