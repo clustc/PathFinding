618a3ed451e69324bebf603df527ceeacbe91f6a
clustc <clustcer@qq.com>
2017-01-23 19:48:12 +0800

    Update BJMPathFinder.h

--- scr/BJMPathFinder.h
+++ scr/BJMPathFinder.h
@@ -9,9 +9,13 @@ Description:
 #include <list>
 #include <vector>
 #include "cocos2d.h"
+#include "BJMPath.h"
 
 namespace BJMGame
 {
+
+#define _MAX(a, b) ((a)>(b) ? (a) : (b))
+#define _MIN(a, b) ((a)<(b) ? (a) : (b))
 //------------------------------------- GridNode -------------------------------------//
 static const short	MAX_MOVE_DIRECTIONS = 8;
 static const short	MOVE_DIRECTION_OFFSETS[MAX_MOVE_DIRECTIONS][2] = 
@@ -57,11 +61,20 @@ struct GridNode
 
 	short x,y;
 	float f,g;
-	bool bOpened, bClosed;	//标记是否在openList列表，或closeList列表
+	bool bOpened, bClosed;	//mark in the openList or the closedList
 	GridNode* pParent;
 };
 
 typedef  std::vector<GridNode*> GridNodeVec;
+
+//simple binary_heap functions
+bool checkBinaryHeapValid(GridNodeVec& vec);
+bool cmp(GridNode* nodeA, GridNode* nodeB);
+void percolate_up(GridNodeVec& vec, int index);
+void percolate_down(GridNodeVec& vec, int index);
+void push(GridNodeVec& vec,GridNode* item);
+GridNode* pop(GridNodeVec& vec);
+void heapify(GridNodeVec& vec, GridNode* item);
 
 class BJMPathFinder
 {
@@ -69,9 +82,6 @@ private:
 	int	m_GridWidth;		//uniform-cost grid maps width
 	int m_GridHeight;		//uniform-cost grid maps height
 	GridNode*** m_grid;
-
-public:
-	std::list<short> m_shortestPath;
 	
 public:
 	BJMPathFinder(void);
@@ -82,21 +92,21 @@ public:
 	bool WalkAble(int x, int y);
 	int	GetDir(int deltaX, int deltaY);
 	GridNode* GetNodeAt(int x, int y);
-	virtual bool PathFinding(const cocos2d::Point & from, const cocos2d::Point & to)=0;
+	
 
-	virtual GridNodeVec FindNeighbours(GridNode* node);
+	virtual void FindNeighbours(GridNodeVec& output, GridNode* node);
 	float GetH(GridNode* nodeA, GridNode* nodeB);
 
-	void PrintPath(GridNode* endNode);
-	bool	HasNextStep();
-	short	GetNextStep();
-	short	WatchNextStep();
-	int		GetLeftStepNum();
-	std::string	GetLeftStepStr();
+	//reset each search node in openList and closedList to origin, it must be called when finding a new path;
+	void	Reset();
+	//remove every newed search node
 	void	Clear();
 
+	//path
+	BJMPath* PrintPath(GridNode* endNode);
+	virtual BJMPath* PathFinding(const cocos2d::Point & from, const cocos2d::Point & to)=0;
 
-	//GetHValue
+	// methods to calculate the Heuristic Value
 	float MANHATTAN(GridNode* nodeA, GridNode* nodeB);
 	float EUCLIDIAN(GridNode* nodeA, GridNode* nodeB);
 	float DIAGONAL(GridNode* nodeA, GridNode* nodeB);
