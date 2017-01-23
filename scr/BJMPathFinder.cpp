726887a4329983099b250be77265d45bbb0c07d7
clustc <clustcer@qq.com>
2017-01-23 19:49:16 +0800

    Update BJMPathFinder.cpp

--- scr/BJMPathFinder.cpp
+++ scr/BJMPathFinder.cpp
@@ -4,6 +4,127 @@
 namespace BJMGame
 {
 
+bool checkBinaryHeapValid(GridNodeVec& vec)
+{
+	//判断第一个是否为最小值
+	if(vec.empty()) return true;
+
+	float min_f = vec[0]->f;
+	for(unsigned i = 1; i < vec.size(); ++i)
+	{
+		if(vec[i]->f < min_f)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+bool cmp(GridNode* nodeA, GridNode* nodeB)
+{
+	return nodeA->f < nodeB->f;
+}
+
+void percolate_up(GridNodeVec& vec, int index)
+{
+	if(index <= 0) return;
+	int parentIndex;
+	if(index%2 == 0)
+	{
+		parentIndex = index/2 -1;
+	}
+	else
+	{
+		parentIndex = (index - 1) /2;
+	}
+
+	if(cmp(vec[index], vec[parentIndex]))
+	{
+		//当前节点权值高于父节点权值（f越小权值越高）,则交换
+		GridNode* pTemp = vec[index];
+		vec[index] = vec[parentIndex];
+		vec[parentIndex] = pTemp;
+
+		//递归
+		percolate_up(vec, parentIndex);
+	}
+}
+
+void percolate_down(GridNodeVec& vec, int index)
+{
+	int lfIndex, rtIndex, minIndex;
+	lfIndex = index*2 + 1;
+	rtIndex = lfIndex + 1;
+	if(rtIndex >= (int)vec.size())
+	{
+		if(lfIndex >= (int)vec.size())
+		{
+			return;
+		}
+		else
+		{
+			minIndex = lfIndex;
+		}
+	}
+	else
+	{
+		if(cmp(vec[lfIndex], vec[rtIndex]))
+		{
+			minIndex = lfIndex;
+		}
+		else
+		{
+			minIndex = rtIndex;
+		}
+	}
+
+	if(cmp(vec[minIndex], vec[index]))
+	{
+		GridNode* pTemp = vec[index];
+		vec[index] = vec[minIndex];
+		vec[minIndex] = pTemp;
+
+		percolate_down(vec, minIndex);
+	}
+}
+
+void push(GridNodeVec& vec,GridNode* item)
+{
+	vec.push_back(item);
+	percolate_up(vec, vec.size()-1);
+}
+
+GridNode* pop(GridNodeVec& vec)
+{
+	if(vec.size() < 1) return NULL;
+	GridNode* root = vec[0];
+	vec[0] = vec[vec.size()-1];
+	vec.pop_back();
+	if(vec.size() > 1)
+	{
+		percolate_down(vec, 0);
+	}
+	return root;
+}
+
+void heapify(GridNodeVec& vec, GridNode* item)
+{
+	int index = -1;
+	for(int i = 0; i<(int)vec.size(); ++i)
+	{
+		if(vec[i] == item)
+		{
+			index = i;
+			break;
+		}
+	}
+	if(index != -1)
+	{
+		percolate_down(vec, index);
+		percolate_up(vec, index);
+	}
+}
+
 
 BJMPathFinder::BJMPathFinder(void)
 	: m_GridWidth(0)
@@ -15,11 +136,47 @@ BJMPathFinder::BJMPathFinder(void)
 
 BJMPathFinder::~BJMPathFinder(void)
 {
-	if(!m_shortestPath.empty())
-	{
-		m_shortestPath.clear();
-	}
+	Clear();
+}
 
+BJMPath* BJMPathFinder::PrintPath( GridNode* endNode )
+{
+	std::vector<short> path;
+	GridNode* node = endNode;
+	while (node->pParent)
+	{
+		GridNode* parent = node->pParent;
+		int dx = node->x - parent->x;
+		int dy = node->y - parent->y;
+
+		path.push_back(GetDir(dx, dy));
+
+		node = parent;
+	}
+	return BJMPath::Create(path);
+}
+
+void BJMPathFinder::Reset()
+{
+	if(m_grid)
+	{
+		for(int i = 0; i < m_GridHeight; ++i )
+		{
+			for(int j = 0; j<m_GridWidth; ++j)
+			{
+				GridNode* node = m_grid[i][j];
+				if(node)
+				{
+					node->Reset();
+				}
+			}
+
+		}
+	}
+}
+
+void BJMPathFinder::Clear()
+{
 	if(m_grid)
 	{
 		for(int i = 0; i < m_GridHeight; ++i )
@@ -37,100 +194,6 @@ BJMPathFinder::~BJMPathFinder(void)
 		}
 		delete []m_grid;
 		m_grid = NULL;
-	}
-}
-
-void BJMPathFinder::PrintPath( GridNode* endNode )
-{
-	if (!m_shortestPath.empty())
-	{
-		m_shortestPath.clear();
-	}
-	GridNode* node = endNode;
-	while (node->pParent)
-	{
-		GridNode* parent = node->pParent;
-		int dx = node->x - parent->x;
-		int dy = node->y - parent->y;
-
-		m_shortestPath.push_back(GetDir(dx, dy));
-
-		node = parent;
-	}
-}
-
-bool BJMPathFinder::HasNextStep()
-{
-	return !m_shortestPath.empty();
-}
-
-short BJMPathFinder::GetNextStep()
-{
-	if(HasNextStep())
-	{
-		short nextPos = m_shortestPath.back();
-		m_shortestPath.pop_back();
-		return nextPos;
-	}
-	else
-	{
-		n_warning("No next step!");
-		return 0;
-	}
-}
-
-int BJMPathFinder::GetLeftStepNum()
-{
-	return m_shortestPath.size();
-}
-
-std::string BJMPathFinder::GetLeftStepStr()
-{
-	if(!m_shortestPath.empty())
-	{
-		std::string result = "";
-		std::list<short>::iterator i; 
-		for (i = m_shortestPath.begin(); i != m_shortestPath.end(); ++i)  
-		{
-			result = result + StringUtils::toString(*i);
-		}
-		return result;
-	}
-	return "";
-}
-
-short BJMPathFinder::WatchNextStep()
-{
-	if(HasNextStep())
-	{
-		short nextPos = m_shortestPath.back();
-		return nextPos;
-	}
-	else
-	{
-		n_warning("No next step!");
-		return 0;
-	}
-}
-
-void BJMPathFinder::Clear()
-{
-	if(!m_shortestPath.empty())
-	{
-		m_shortestPath.clear();
-	}
-
-	for(int i = 0; i < m_GridHeight; ++i )
-	{
-		for(int j = 0; j<m_GridWidth; ++j)
-		{
-			GridNode* node = m_grid[i][j];
-			if(node)
-			{
-				node->Reset();
-			}
-		}
-
 	}
 }
 
@@ -162,22 +225,28 @@ bool BJMGame::BJMPathFinder::WalkAble( int x, int y )
 	return  !bBlocked;
 }
 
-BJMGame::GridNodeVec BJMGame::BJMPathFinder::FindNeighbours( GridNode* node )
+void BJMGame::BJMPathFinder::FindNeighbours(GridNodeVec& output, GridNode* node)
 {
-	GridNodeVec vec;
+	if(!output.empty())
+	{
+		output.clear();
+	}
+	
+	int Indexes[MAX_MOVE_DIRECTIONS] = {0,2,4,6,1,3,5,7};
+
 	for(int i = 0; i<MAX_MOVE_DIRECTIONS; i++)
 	{
-		int x = node->x + BJMGame::MOVE_DIRECTION_OFFSETS[i][0];
-		int y = node->y + BJMGame::MOVE_DIRECTION_OFFSETS[i][1];
+		int index = Indexes[i];
+		int x = node->x + BJMGame::MOVE_DIRECTION_OFFSETS[index][0];
+		int y = node->y + BJMGame::MOVE_DIRECTION_OFFSETS[index][1];
 
 		GridNode* pNode = GetNodeAt(x,y);
-		if(pNode)
+		if(pNode && WalkAble(x,y))
 		{
-			vec.push_back(pNode);
+			output.push_back(pNode);
 		}
 
 	}
-	return vec;
 }
 
 int BJMGame::BJMPathFinder::GetDir( int deltaX, int deltaY )
@@ -201,7 +270,7 @@ GridNode* BJMGame::BJMPathFinder::GetNodeAt(int x, int y)
 
 	if(!m_grid[y][x])
 	{
-		m_grid[y][x] = n_new(GridNode(x,y));
+		m_grid[y][x] = new GridNode(x,y);
 	}
 	return m_grid[y][x];
 }
